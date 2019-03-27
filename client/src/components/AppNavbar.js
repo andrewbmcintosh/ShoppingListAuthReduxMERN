9b02f298ba4359be638dc3cada69486d176a12a8
andrewmcintosh <abmcintosh7@gmail.com>
2019-03-27 16:28:52 -0400

    building out frontend

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,1 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+export class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expands="sm" className="mb-5">
+          <Container>
+            <NavbarBrand>ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/andrewbmcintosh">
+                    Github
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
